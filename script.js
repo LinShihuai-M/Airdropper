6dc2c271607f1e4dda449104cd33c63187e24bc0
LinShihuai-M <linxiansheng077@gmail.com>
2025-11-15 23:01:02 +0800

    更新 script.js

--- script.js
+++ script.js
@@ -1,26 +1,12 @@
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-let signer;
+const hre = require("hardhat");
 
-async function connect() {
-  await provider.send("eth_requestAccounts", []);
-  signer = provider.getSigner();
+async function main() {
+  const Airdropper = await hre.ethers.deployContract("Airdropper");
+  await Airdropper.waitForDeployment();
+  console.log("Airdropper deployed to:", await Airdropper.getAddress());
 }
-connect();
 
-document.getElementById("airdrop").onclick = async () => {
-  const token = document.getElementById("token").value.trim();
-  const recipients = document.getElementById("recipients").value.split(",").map(a => a.trim()).filter(a => a);
-  const totalAmount = document.getElementById("amount").value;
-
-  const contractAddress = "<PUT_YOUR_CONTRACT_ADDRESS_HERE>";
-  const abi = [
-    "function airdropTokens(address token, address[] calldata recipients, uint256 totalAmount) external"
-  ];
-
-  const contract = new ethers.Contract(contractAddress, abi, signer);
-
-  const tx = await contract.airdropTokens(token, recipients, totalAmount);
-  await tx.wait();
-
-  alert("Airdrop Completed!");
-};
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
